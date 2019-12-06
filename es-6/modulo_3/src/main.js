dbb44236862b5fb2bfd53cd500a53ff3f08c4bf6
Arnaldo Batista <arnaldo.1565@hotmail.com>
2019-12-06 11:52:41 -0300

    curso skylab.rocketseat

--- es-6/modulo_3/src/main.js
+++ es-6/modulo_3/src/main.js
@@ -1,38 +1,3 @@
-// const minhaPromise = () => new Promise((resolve, reject) => {  
-//     setTimeout (() => { resolve('ok') }, 2000);
-// })
-
-// // async function executaPromise(){
-// //     console.log (await minhaPromise());
-// //     console.log (await minhaPromise());
-// //     console.log (await minhaPromise());
-// // }
-
-// const executaPromise = async () => {
-//     console.log (await minhaPromise());
-//     console.log (await minhaPromise());
-//     console.log (await minhaPromise());
-// };
-
-// executaPromise();
-
-// import axios from 'axios';
-
-// class Api {
-//     static async getUserInfo(username) {
-//         try{
-//             const response = await axios.get(`https://api.github.com/users/${username}`);
-//             console.log(response);
-//         }
-//         catch(err){
-//             console.warn('Erro'); 
-//         }
-//     }
-// }
-
-// Api.getUserInfo('diego3gfjsldkjfsdlfskl');
-
-// Funão delay aciona o .then após 1s
 const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 const umPorSegundo = async () => {
     await delay(console.log('1s'));
